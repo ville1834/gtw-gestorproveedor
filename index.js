67eefc3e17f8c508e564f55988baea854e2e5e56
Josue <ville_1834@hotmail.com>
2020-05-15 09:59:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ fastify.post('/gateway-management', function (req, reply) {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(8080)
+    await fastify.listen(8080,'0.0.0.0')
     fastify.log.info(`server listening on ${fastify.server.address().port}`)
   } catch (err) {
     fastify.log.error(err)
