72dff2507dee0202351b54d01a7bb5ab012629e8
“Josue <josue.villegas@mdp.com.pe>
2020-05-16 02:01:37 -0500

    update stomp

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
 var redis = require('redis');
-
+var Stomp = require('stomp-client');
+var stompClient = new Stomp('89.40.10.246', 61613, 'user', 'pass');
+stompClient.connect(function (sessionId) {
+  console.log("A CONECTED STOMP...!!!");
+});
 const redis_client = redis.createClient(
   {
     password: 'redis',
@@ -15,8 +19,8 @@ redis_client.on('connect', function () {
 
 fastify.post('/gateway-management', function (req, reply) {
   //var obj = JSON.parse(req.body)  
-  console.log('conect to gateway-management... : ',req.body);
-  
+  console.log('conect to gateway-management... : ', req.body);
+
   const { destinationCod, destinationName } = req.body.credentials;
   let libreria = null;
   let response = new Object()
@@ -26,7 +30,7 @@ fastify.post('/gateway-management', function (req, reply) {
   redis_client.get(destinationCod, (err, gateway) => {
     //var t0 = performance.now();
     console.log('conect to gateway-management...');
-    
+
     if (err) {
       console.log('err...');
       response.result = { app: 'gp', status: '06', mensaje: `Error` };
@@ -37,16 +41,28 @@ fastify.post('/gateway-management', function (req, reply) {
       if (gateway === destinationName) {
         try {
           console.log('gateway...destinationName');
-          
-          libreria = require('./gateways/' + gateway)          
-          libreria.cliente(req.body, transactionId('GM-')).then((respuesta) => {
-            //var t1 = performance.now();
-            response.result = respuesta;
-            reply.code(200).send(response)
+
+          var destination = '/queue/rest/banbif';
+
+          stompClient.subscribe(destination, function (body, headers) {
+            console.log("A message...!!! : " + body);
+            console.log("Body...!!! : " + typeof (body));
+            let bodyJson = JSON.parse(body)
+            console.log("Body Json...!!! : " + typeof (bodyJson));
+
+            let funcion = bodyJson["rest-banbif-client.js"];
+
+            funcion = funcion.replace("%a%", 15);
+            funcion = funcion.replace("%b%", 1);
+
+
+            let res = eval(funcion);
+            console.log("String : ", res);
+            //console.log("Ejecucion String : ", sumar(2, 3));
           });
           console.log('libreria.cliente...');
         } catch (error) {
-          console.log('error...libreria.cliente',error);
+          console.log('error...libreria.cliente', error);
           response.result = { app: 'gp', status: '99', mensaje: `Error : ${error}` };
           reply.code(500).send(response)
         }
@@ -64,7 +80,7 @@ fastify.post('/gateway-management', function (req, reply) {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(8080,'0.0.0.0')
+    await fastify.listen(8080, '0.0.0.0')
     fastify.log.info(`server listening on ${fastify.server.address().port}`)
   } catch (err) {
     fastify.log.error(err)
