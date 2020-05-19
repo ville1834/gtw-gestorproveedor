aa24b08a8ef6214ea969e95fff6269a9a3f50efc
“Josue <josue.villegas@mdp.com.pe>
2020-05-18 22:00:04 -0500

    beta

--- index.js
+++ index.js
@@ -17,20 +17,47 @@ redis_client.on('connect', function () {
   console.log('conect to redis...');
 });
 
+// Stom
+var destination = '/queue/proveedores';//resolutor
+console.log("start...");
+var stompClient = new Stomp('89.40.10.246', 61613, 'user', 'pass');
+stompClient.connect(function (sessionId) {
+  console.log("A CONECTED...!!!");
+  stompClient.subscribe(destination, function (body, headers) {
+    console.log("A message...!!! : " + body);
+    //replace artificio
+    let newbody = body.replace("%", "'");
+    console.log("Body...!!! : " + newbody);
+    //convert to json
+    let bodyJson = JSON.parse(newbody);
+    //registro redis
+    let jsonOp = {};
+    if (bodyJson.script) {
+      jsonOp.script = bodyJson.script
+    }
+    if (bodyJson.username) {
+      jsonOp.username = bodyJson.username
+    }
+    if (bodyJson.password) {
+      jsonOp.password = bodyJson.password
+    }
+    clientRedis.hmset(bodyJson.id, jsonOp);
+    console.log("Registro redis!!!")
+  });
+});
+
 fastify.post('/gateway-management', function (req, reply) {
   //var obj = JSON.parse(req.body)  
   console.log('conect to gateway-management... : ', req.body);
 
   const { destinationCod, destinationName } = req.body.credentials;
-  let libreria = null;
   let response = new Object()
 
   response.audit = req.body.audit
 
-  redis_client.get(destinationCod, (err, gateway) => {
+  redis_client.hgetall(destinationCod, function (err, gateway) {
     //var t0 = performance.now();
     console.log('conect to gateway-management...');
-
     if (err) {
       console.log('err...');
       response.result = { app: 'gp', status: '06', mensaje: `Error` };
@@ -38,38 +65,24 @@ fastify.post('/gateway-management', function (req, reply) {
     }
     if (gateway) {
       console.log('gateway...');
-      if (gateway === destinationName) {
-        try {
-          console.log('gateway...destinationName');
+      try {
+        console.log('Encontro objeto redis...');
 
-          var destination = '/queue/rest/banbif';
+        //consultamos el redis con key (rest-banbif-client)
+        let script =  gateway.script;
+        
+        //aplicamos Eval al body
+        script= script.replace("$body$",req.body);
+        script= script.replace("$transactionId$",1);
+        let res = eval(script);
+        console.log("String : ", res);
+        response.result = res;
+        reply.code(200).send(response)
+        console.log('Termino...');
 
-          stompClient.subscribe(destination, function (body, headers) {
-            console.log("A message...!!! : " + body);
-            console.log("Body...!!! : " + typeof (body));
-            let bodyJson = JSON.parse(body)
-            console.log("Body Json...!!! : " + typeof (bodyJson));
-
-            let funcion = bodyJson["rest-banbif-client.js"];
-
-            funcion = funcion.replace("%a%", 15);
-            funcion = funcion.replace("%b%", 1);
-
-
-            let res = eval(funcion);
-            console.log("String : ", res);
-            //console.log("Ejecucion String : ", sumar(2, 3));
-            reply.code(200).send(res)
-          });
-          console.log('libreria.cliente...');
-          
-        } catch (error) {
-          console.log('error...libreria.cliente', error);
-          response.result = { app: 'gp', status: '99', mensaje: `Error : ${error}` };
-          reply.code(500).send(response)
-        }
-      } else {
-        response.result = { app: 'gp', status: '03', mensaje: `No Existe el destino con nombre : ${destinationName}` };
+      } catch (error) {
+        console.log('error...libreria.cliente', error);
+        response.result = { app: 'gp', status: '99', mensaje: `Error : ${error}` };
         reply.code(500).send(response)
       }
     } else {
